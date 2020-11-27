8afe67b1ee45eb053a3e0d8dd00a59cb7be7729b
zhengzhendu <zdzhengzhendu@163.com>
2020-11-28 00:00:03 +0800

    订单支付实时监控,初始化数据

---
+++ OrderTimeoutDetect/src/main/scala/OrderTimeout.scala
@@ -0,0 +1,27 @@
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+
+
+//定义输入输出样例类类型
+case class OrderEvent(orderId: Long, eventType: String, txId: String, timestamp: Long)
+
+case class OrderResult(orderId: Long, resultMsg: String)
+
+object OrderTimeout {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    //读取数据
+    val resource = getClass.getResource("/OrderLog.csv")
+    val orderEventStream = env.readTextFile(resource.getPath)
+      .map(data => {
+        val arr = data.split(",")
+        OrderEvent(arr(0).toLong, arr(1), arr(2), arr(3).toLong)
+      })
+      .assignAscendingTimestamps(_.timestamp)
+
+
+  }
+}
