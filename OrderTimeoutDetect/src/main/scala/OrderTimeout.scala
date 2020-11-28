b27e5e0ef35e5fa0a9aed41c8c359ca0473a4a07
zhengzhendu <zdzhengzhendu@163.com>
2020-11-28 22:31:15 +0800

    订单支付实时监控,打印15分钟内没支付的订单

--- OrderTimeoutDetect/src/main/scala/OrderTimeout.scala
+++ OrderTimeoutDetect/src/main/scala/OrderTimeout.scala
@@ -1,5 +1,11 @@
+import java.util
+
+import org.apache.flink.cep.{PatternSelectFunction, PatternTimeoutFunction}
+import org.apache.flink.cep.scala.CEP
+import org.apache.flink.cep.scala.pattern.Pattern
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
 
 
 //定义输入输出样例类类型
@@ -23,8 +29,44 @@ object OrderTimeout {
       .assignAscendingTimestamps(_.timestamp * 1000L)
       .keyBy(_.orderId)
 
-    //
+    // 定义pattern
+    val orderPayPattern = Pattern
+      .begin[OrderEvent]("create").where(_.eventType == "create")
+      .followedBy("pay").where(_.eventType == "pay")
+      .within(Time.minutes(15))
 
 
+    // 将pattern应用到数据流上，进行模式匹配
+    val patternStream = CEP.pattern(orderEventStream, orderPayPattern)
+
+    // 定义侧输出流标签，用于处理超时事件
+    val orderTimeoutOutputTag = new OutputTag[OrderResult]("orderTimeout")
+
+    // 调用select方法，提取并处理匹配的成功支付的事件以及超时事件
+    val resultStream = patternStream.select(orderTimeoutOutputTag,
+      new OrderTimeoutSelect(),
+      new OrderPaySelect()
+    )
+
+    resultStream.print()
+    resultStream.getSideOutput(orderTimeoutOutputTag).print("timeout")
+
+    env.execute("order timeout job")
+  }
+}
+
+
+class OrderTimeoutSelect() extends PatternTimeoutFunction[OrderEvent, OrderResult] {
+  override def timeout(map: util.Map[String, util.List[OrderEvent]], timeoutTimestamp: Long): OrderResult = {
+    val timeoutOrderId = map.get("create").iterator().next().orderId
+    OrderResult(timeoutOrderId, "timeout :" + timeoutTimestamp)
+  }
+}
+
+
+class OrderPaySelect() extends PatternSelectFunction[OrderEvent, OrderResult] {
+  override def select(map: util.Map[String, util.List[OrderEvent]]): OrderResult = {
+    val payedOrderId = map.get("pay").iterator().next().orderId
+    OrderResult(payedOrderId, "already payed")
   }
 }
